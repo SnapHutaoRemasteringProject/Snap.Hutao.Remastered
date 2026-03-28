8d41682c1260d5fff8dad14a82834d3033314c01
hoshiizumiya <Hoshiizumiya@outlook.com>
2026-03-28 22:50:32 +0800

    Add advanced start.

--- src/Snap.Hutao.Remastered/Snap.Hutao.Remastered/Service/Game/LaunchOptions.cs
+++ src/Snap.Hutao.Remastered/Snap.Hutao.Remastered/Service/Game/LaunchOptions.cs
@@ -177,6 +177,15 @@ public sealed partial class LaunchOptions : DbStoreOptions, IRestrictedGamePathA
     [field: MaybeNull]
     public IObservableProperty<bool> UsingOverlay { get => field ??= CreateProperty(SettingKeys.LaunchUsingOverlay, false); }
 
+    [field: MaybeNull]
+    public IObservableProperty<string> AdvancedStartProgramPath { get => field ??= CreateProperty(SettingKeys.LaunchAdvancedStartProgramPath, string.Empty); }
+
+    [field: MaybeNull]
+    public IObservableProperty<bool> AdvancedStartDelayedOnAdvancedStart { get => field ??= CreateProperty(SettingKeys.LaunchAdvancedStartDelayedOnAdvancedStart, false); }
+
+    [field: MaybeNull]
+    public IObservableProperty<bool> AdvancedStartDelayedOnGameLaunch { get => field ??= CreateProperty(SettingKeys.LaunchAdvancedStartDelayedOnGameLaunch, false); }
+
     private static int InitializeTargetFpsWithScreenFps()
     {
         return HutaoNative.Instance.MakeDeviceCapabilities().GetPrimaryScreenVerticalRefreshRate();
