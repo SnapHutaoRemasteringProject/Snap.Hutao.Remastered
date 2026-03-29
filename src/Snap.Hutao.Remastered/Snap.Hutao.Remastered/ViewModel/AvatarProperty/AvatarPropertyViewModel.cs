93c2e1ac4b3a17b60d2d8466e6b0600477373416
nahida-mouo <nahida-mouo@outlook.com>
2026-03-29 16:31:08 +0800

    在“我的角色”中添加批量导出（角色练度情况）文本为文本文档 #82

--- src/Snap.Hutao.Remastered/Snap.Hutao.Remastered/ViewModel/AvatarProperty/AvatarPropertyViewModel.cs
+++ src/Snap.Hutao.Remastered/Snap.Hutao.Remastered/ViewModel/AvatarProperty/AvatarPropertyViewModel.cs
@@ -26,6 +26,7 @@ using Snap.Hutao.Remastered.Web.Hoyolab.Takumi.Event.Calculate;
 using System.Collections.Immutable;
 using System.Globalization;
 using System.Runtime.CompilerServices;
+using System.Text;
 using CalculatorAvatarPromotionDelta = Snap.Hutao.Remastered.Web.Hoyolab.Takumi.Event.Calculate.AvatarPromotionDelta;
 using CalculatorBatchConsumption = Snap.Hutao.Remastered.Web.Hoyolab.Takumi.Event.Calculate.BatchConsumption;
 using CalculatorConsumption = Snap.Hutao.Remastered.Web.Hoyolab.Takumi.Event.Calculate.Consumption;
@@ -519,6 +520,30 @@ public sealed partial class AvatarPropertyViewModel : Abstraction.ViewModel, IRe
         }
 
         InfoBarMessage message = await scopeContext.ClipboardProvider.SetTextAsync(AvatarViewTextTemplating.GetTemplatedText(avatar)).ConfigureAwait(false)
+            ? InfoBarMessage.Success(SH.ViewModelAvatatPropertyExportTextSuccess)
+            : InfoBarMessage.Warning(SH.ViewModelAvatatPropertyExportTextError);
+
+        scopeContext.Messenger.Send(message);
+    }
+
+    [Command("ExportAllToTextCommand")]
+    private async Task ExportAllToTextAsync()
+    {
+        SentrySdk.AddBreadcrumb(BreadcrumbFactory.CreateUI("Export all as text to ClipBoard", "AvatarPropertyViewModel.Command"));
+
+        if (Summary is not { Avatars: { } avatars })
+        {
+            return;
+        }
+
+        StringBuilder stringBuilder = new();
+
+        foreach (AvatarView avatar in avatars)
+        {
+            stringBuilder.AppendLine(AvatarViewTextTemplating.GetTemplatedText(avatar));
+        }
+
+        InfoBarMessage message = await scopeContext.ClipboardProvider.SetTextAsync(stringBuilder.ToString()).ConfigureAwait(false)
             ? InfoBarMessage.Success(SH.ViewModelAvatatPropertyExportTextSuccess)
             : InfoBarMessage.Warning(SH.ViewModelAvatatPropertyExportTextError);
 
