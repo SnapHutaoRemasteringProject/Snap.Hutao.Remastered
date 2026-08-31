5605083d5a82b37874648bc57b8de3636dc5a691
nahida-mouo <nahida-mouo@outlook.com>
2026-08-31 16:00:03 +0800

    移除CountdownConfirmDialog中不必要的Using

--- src/Snap.Hutao.Remastered/Snap.Hutao.Remastered/UI/Xaml/View/Dialog/CountdownConfirmDialog.xaml.cs
+++ src/Snap.Hutao.Remastered/Snap.Hutao.Remastered/UI/Xaml/View/Dialog/CountdownConfirmDialog.xaml.cs
@@ -2,7 +2,6 @@
 // Licensed under the MIT license.
 
 using Microsoft.UI.Dispatching;
-using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Snap.Hutao.Remastered.Factory.ContentDialog;
 
