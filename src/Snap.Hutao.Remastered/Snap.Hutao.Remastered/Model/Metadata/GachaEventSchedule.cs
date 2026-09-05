509b47528ccf5acd310f9423869ee24e356a72de
nahida-mouo <nahida-mouo@outlook.com>
2026-09-06 07:21:24 +0800

    修正所有副本开放天数

--- src/Snap.Hutao.Remastered/Snap.Hutao.Remastered/Model/Metadata/GachaEventSchedule.cs
+++ src/Snap.Hutao.Remastered/Snap.Hutao.Remastered/Model/Metadata/GachaEventSchedule.cs
@@ -8,13 +8,11 @@ namespace Snap.Hutao.Remastered.Model.Metadata;
 
 public static class GachaEventSchedule
 {
-    // 换卡池当天算第 0 天，其后 7 天仍全开，含当天共 8 个自然日。
-    private const int AllMaterialsOpenWindowLengthInDays = 8;
+    private const int AllMaterialsOpenWindowLengthInDays = 7;
 
     /// <summary>
     /// 判断指定服务器本地日期是否处于某个"角色卡池开启后素材副本全天全开"窗口内。
     /// 触发卡池：角色活动祈愿(301) 与 特殊角色活动祈愿(400)，其 From 即版本更新/换卡池起点。
-    /// 窗口 = 开启当天(第 0 天) + 其后 7 天。
     /// </summary>
     public static bool IsDateInAllMaterialsOpenWindow(this ImmutableArray<GachaEvent> events, in DateOnly date, in TimeSpan serverTimeZoneOffset)
     {
